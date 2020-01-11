1c333cdcbb5444254b2a24aa76eafc8d578afd00
Hankyu Lee <aicies119@gmail.com>
2020-01-11 17:06:01 +0900

    Step 11 - add a recyclerview

--- app/src/main/java/pe/hankyu/roomwordsample/MainActivity.kt
+++ app/src/main/java/pe/hankyu/roomwordsample/MainActivity.kt
@@ -2,11 +2,20 @@ package pe.hankyu.roomwordsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import pe.hankyu.roomwordsample.adapter.WordListAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = WordListAdapter(this)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview).apply {
+            this.adapter = adapter
+            layoutManager = LinearLayoutManager(this@MainActivity)
+        }
     }
 }
---
+++ app/src/main/java/pe/hankyu/roomwordsample/adapter/WordListAdapter.kt
@@ -0,0 +1,39 @@
+package pe.hankyu.roomwordsample.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import pe.hankyu.roomwordsample.R
+import pe.hankyu.roomwordsample.data.Word
+
+class WordListAdapter internal constructor(
+    context: Context
+) : RecyclerView.Adapter<WordListAdapter.WordViewHolder>() {
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+    private var words = emptyList<Word>()
+
+    inner class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val wordItemView: TextView = itemView.findViewById(R.id.textView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        val itemView = inflater.inflate(R.layout.recyclerview_item, parent, false)
+        return WordViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        val current = words[position]
+        holder.wordItemView.text = current.word
+    }
+
+    internal fun setWords(words: List<Word>) {
+        this.words = words
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount() = words.size
+
+}

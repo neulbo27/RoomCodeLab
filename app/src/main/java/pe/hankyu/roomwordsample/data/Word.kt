12262e3d6338a6e8e08eb6c61643fe02e28a17a4
Hankyu Lee <aicies119@gmail.com>
2020-01-11 16:05:04 +0900

    Step 4 - Word data class added

---
+++ app/src/main/java/pe/hankyu/roomwordsample/data/Word.kt
@@ -0,0 +1,8 @@
+package pe.hankyu.roomwordsample.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+data class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)

57d3bafe1fa2ee4e46faeadb901568713c6f2666
Hankyu Lee <aicies119@gmail.com>
2020-01-11 16:05:28 +0900

    Step 5 - DAO class added

---
+++ app/src/main/java/pe/hankyu/roomwordsample/data/WordDao.kt
@@ -0,0 +1,18 @@
+package pe.hankyu.roomwordsample.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface WordDao {
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    fun getAlphabetizedWords(): List<Word>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    suspend fun deleteAll()
+}

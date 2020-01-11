b0986830548656e59ad0e1f3d325ba292628a0b2
Hankyu Lee <aicies119@gmail.com>
2020-01-11 16:01:53 +0900

    initial commit & repository dependency added.

---
+++ app/src/main/java/pe/hankyu/roomwordsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package pe.hankyu.roomwordsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

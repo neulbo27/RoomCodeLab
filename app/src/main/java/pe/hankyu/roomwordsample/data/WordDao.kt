ab0b576108bb387b3aa9f69d59e77f193861a2cf
Hankyu Lee <aicies119@gmail.com>
2020-01-11 16:23:37 +0900

    Step 8 - implementation Repository class, Dao getAlphabetizedWords function return type modified

--- app/src/main/java/pe/hankyu/roomwordsample/data/WordDao.kt
+++ app/src/main/java/pe/hankyu/roomwordsample/data/WordDao.kt
@@ -1,5 +1,6 @@
 package pe.hankyu.roomwordsample.data
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -8,7 +9,7 @@ import androidx.room.Query
 @Dao
 interface WordDao {
     @Query("SELECT * from word_table ORDER BY word ASC")
-    fun getAlphabetizedWords(): List<Word>
+    fun getAlphabetizedWords(): LiveData<List<Word>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(word: Word)

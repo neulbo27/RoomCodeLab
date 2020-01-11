92aeed2e31d5807022dc52f3aea399a7df3da593
Hankyu Lee <aicies119@gmail.com>
2020-01-11 17:29:55 +0900

    Step 13 - NewWordActivity(add word) added

---
+++ app/src/main/java/pe/hankyu/roomwordsample/NewWordActivity.kt
@@ -0,0 +1,36 @@
+package pe.hankyu.roomwordsample
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class NewWordActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_REPLY = "pe.hankyu.wordlistsql.REPLY"
+    }
+
+    private val editTextView by lazy { findViewById<EditText>(R.id.edit_word) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_word)
+
+        val button = findViewById<Button>(R.id.button_save)
+        button.setOnClickListener {
+            val replyIntent = Intent()
+            if (editTextView.text.isEmpty()) {
+                setResult(Activity.RESULT_CANCELED, replyIntent)
+            } else {
+                val word = editTextView.text.toString()
+                replyIntent.putExtra(EXTRA_REPLY, word)
+                setResult(Activity.RESULT_OK, replyIntent)
+            }
+
+            finish()
+        }
+    }
+}
